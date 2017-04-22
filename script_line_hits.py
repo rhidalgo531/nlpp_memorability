2b863fb87c8a8438a22a5a841ec158483d050fe4
Raify <rmh391@nyu.edu>
2017-04-22 16:48:53 -0400

    Sentences filtered, scores scraped

---
+++ script_line_hits.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+import urllib2
+from urllib2 import urlopen
+import re
+import math
+import time
+
+yahoo_search_url_prefix = '''https://search.yahoo.com/search?q='''
+#yahoo_search_url_prefix = '''https://www.bing.com/search?q='''
+get_total_results = re.compile('<span[^>]*> *([0-9,]+) results *</span>')
+webscore_dict = {}
+
+def replace_spaces_with_plus (term):
+    return(term.replace(' ','+'))
+
+def do_provider_search(term):
+    url = yahoo_search_url_prefix + '"'+replace_spaces_with_plus(term)+'"'
+    url_stream = urlopen(url)
+    data = str(url_stream.read())
+    url_stream.close()
+    return(data)
+
+def do_provider_search_with_pause(term,timing=1,reps=0):
+    ## without this function, the system will halt every time
+    ## the internet connection is interupted
+    if reps>0:
+        print('Internet search failure')
+        output = False
+        total = -1
+    else:
+        try:
+            output = do_provider_search(term)
+            total = str(get_total_results.search(output).group(1).replace(',',''))
+        except:
+            print('Temporary internet search failure. Trying again')
+            time.sleep(timing)
+            total = do_provider_search_with_pause(term,timing=timing,reps=reps+1)
+    return(total)
+
+
+if __name__ == "__main__":
+    main()
---
+++ strip_movie_sentences.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+# Use: take screenplays of movies, with various formatting in whitespace and
+# extract the sentences into an output file, one sentence per line
+import sys
+import re
+import script_line_hits
+
+def output_text(script, output, score_output, text):
+    for line in script:
+        output.write(line + "\n")
+        score_line = line + ","
+        if score_line not in text:
+            data = script_line_hits.do_provider_search_with_pause(line, timing=1, reps=0)
+            if (data >= 100000):
+                score_line = line + "," + data + "\n"
+                score_output.write(score_line)
+            #elif data == -1:
+            #    score_output.write(score_line + "\n")
+    print 1
+    output.write("THE END\n")
+
+def remove_extra_spaces(doc):
+    for i in range(len(doc)):
+        line = doc[i]
+        sentence = ""
+        for word in line.split(" "):
+            word.strip()
+            if word != '':
+                sentence+= word + " "
+        doc[i] = sentence
+    return doc
+
+def main():
+    file_to_open = "raw_movie_scripts.txt"
+    raw_movie_scripts = open(file_to_open, 'r')
+    text = raw_movie_scripts.read()
+    all_movie_scripts = text.split("THE END")
+    output = open("filtered_movie_script_sentences.txt", 'w+');
+    score_output = open("movie_script_sentences_scores.txt", 'a+');
+    score_output.seek(0)
+    text = score_output.read()
+    score_output.seek(len(text) - 1)
+    for i in range(len(all_movie_scripts) - 1):
+        split_text = all_movie_scripts[i].replace("\n", " ")
+        sentences = re.split(r'(?<=[^A-Z].[.?]) +(?=[A-Z])', split_text)
+        filtered_sentences = remove_extra_spaces(sentences)
+        output_text(filtered_sentences, output, score_output, text)
+    output.close()
+
+if __name__ == "__main__":
+    main()
+    #sys.exit(main(sys.argv));
